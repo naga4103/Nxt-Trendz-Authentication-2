5a99faae0f9b9891628da412882e152f1bb44ec0
naga4103 <pulugunaga4103@gmail.com>
2022-10-12 10:29:33 +0000

    Commit1

---
+++ src/components/Cart/index.js
@@ -0,0 +1,28 @@
+// Write your JS code here
+
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+import './index.css'
+import Header from '../Header'
+
+const Cart = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="cart-image-container">
+        <img
+          className="cart-image"
+          alt="cart"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        />
+      </div>
+    </>
+  )
+}
+export default Cart
